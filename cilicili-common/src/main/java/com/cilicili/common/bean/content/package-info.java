d648bee1b0e4f3b7a51c1df238d50e8f4fa06b9d
living2room <living2room@sina.com>
2019-05-17 09:01:56 +0800

    每个模块建立Controller、service、mapper包
    持久化bean放在common包下com.cilicili.common.bean.模块名 内

---
+++ cilicili-common/src/main/java/com/cilicili/common/bean/content/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**content模块持久层
+ * @author 李明睿
+ *
+ */
+package com.cilicili.common.bean.content;
---
+++ cilicili-user/src/main/java/com/cilicili/user/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.cilicili.user.controller;
