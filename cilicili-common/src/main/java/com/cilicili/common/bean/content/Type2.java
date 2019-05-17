34a12f20fe048a67f67676b4c2eee4f2e8a4f545
living2room <living2room@sina.com>
2019-05-17 18:36:11 +0800

    三层测试

---
+++ cilicili-common/src/main/java/com/cilicili/common/bean/content/Type2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.cilicili.common.bean.content;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import lombok.Data;
+
+/**
+ * @author 李明睿
+ * 2019年5月17日
+ */
+@Data
+@TableName("type")
+public class Type2 {
+	private Integer id;
+	
+}
---
+++ cilicili-content/src/main/java/com/cilicili/content/mapper/TypeMapper.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.cilicili.content.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.tomcat.util.modeler.BaseModelMBean;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cilicili.common.bean.content.Type2;
+/**
+ * @author 李明睿
+ * 2019年5月17日
+ */
+@Mapper
+public interface TypeMapper extends BaseMapper<Type2>{
+
+}
