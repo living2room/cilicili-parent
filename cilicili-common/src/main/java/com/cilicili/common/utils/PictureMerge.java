dd7dfd4f95a21de2a7b847f09b598798b39cf8ca
living2room <19966117+living2room@users.noreply.github.com>
2019-05-18 21:14:40 +0800

    图片合并工具包OK

--- cilicili-common/src/main/java/com/cilicili/common/utils/PictureMerge.java
+++ cilicili-common/src/main/java/com/cilicili/common/utils/PictureMerge.java
@@ -4,162 +4,112 @@
 package com.cilicili.common.utils;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import cn.hutool.core.img.ImgUtil;
+import cn.hutool.core.io.FileUtil;
+
 /**
  * 合并多张图片
+ * 
  * @author 李明睿 2019年5月18日
  */
 public class PictureMerge {
-	
-	 private Graphics2D g = null;  
-	 private static final int orientation = 0;
-	 private static final int portrait = 1;
-	 //orientation 横向
-	 //portrait 纵向
 
-     
+	private Graphics2D g = null;
+	/**
+	 * orientation 横向
+	 */
+	private static final int orientation = 0;
+	/**
+	 * portrait 纵向
+	 */
+	private static final int portrait = 1;
 
-	    /**导入图片到缓存区
-	     * @param imgName 图片路径
-	     * @return 
-	     */
-	    public BufferedImage loadImageLocal(String imgName) {  
-	        try {  
-	            return ImageIO.read(new File(imgName));  
-	        } catch (IOException e) {  
-	            System.out.println(e.getMessage());  
-	        }  
-	        return null;  
-	    } 
-	    /**合并多张图片为一张图片，指定其高和宽
-	     * @param bi 第一张图片
-	     * @param bis 其他图片
-	     * @param height 指定图片高度
-	     * @param width 指定图片宽度
-	     * @param status 合并图片的方向 orientation=0 and portrait=1.
-	     * @return
-	     */
-	    public BufferedImage Merge(BufferedImage bi,int height, int width,int status,BufferedImage... bis) {  
-	    	//W:72 H:41
-	    	int w = 0;  int h = 0;
-	    	g = bi.createGraphics();
-	    	g.drawImage(bi, w, h,width, height, null);
-	    	for (int i = 0; i < bis.length; i++) {
-	    		try {  
-	    			if (status == orientation ) {w = w + width;}
-	    			else if(status == portrait){h = h + height;}
-	    			else { new Exception("status异常;你到底想往哪边合并啊");}
-	    			g.drawImage(bis[i], w, h,width, height, null);  
-	    		} catch (Exception e) {  
-	    			System.out.println(e.getMessage());  
-	    		}  
+	/**
+	 * 导入图片到缓存区
+	 * 
+	 * @param imgName 图片路径
+	 * @return
+	 */
+	public BufferedImage loadImageLocal(String imgName) {
+		try {
+			return ImageIO.read(new File(imgName));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+
+	/**合并多张图片
+	 * @param status 横向还是纵向
+	 * @param bis 多张图片
+	 * @return
+	 */
+	public BufferedImage Merge(int status, BufferedImage... bis) {
+		// W:72 H:41
+		int w = 0;
+		int h = 0;
+		int height =0;
+		int width = 0;
+		for (int i = 0; i < bis.length; i++) {
+			if (status == orientation) {
+				width = width + bis[i].getWidth();
+			} else if (status == portrait) {
+				height = height + bis[i].getHeight();
+			} else {
+				new Exception("status异常;你到底想往哪边合并啊");
 			}
-	    	g.dispose();  
-	  
-	        return bi;  
-	    } 
-	    
-	    public BufferedImage Merge(BufferedImage b, BufferedImage d) {  
-	        
-	        try {  
-	            int w = b.getWidth();  
-	            int h = b.getHeight();  
-	  
-	            g = d.createGraphics();  
-	            g.drawImage(b, 300, -800, w, h, null);  
-	            g.dispose();  
-	        } catch (Exception e) {  
-	            System.out.println(e.getMessage());  
-	        }  
-	        return d;  
-	    } 
-	    
-	    
-	    /**合成多张图片为一张图片,指定其宽
-	     * @param bi 第一张图片
-	     * @param width 指定宽度
-	     * @param bis 其他图片
-	     * @param status 合并图片的方向 orientation=0 and portrait=1.
-	     * @return
-	     */
-	    public BufferedImage MergeWidth(BufferedImage bi,int width,int status,BufferedImage... bis) {  
-	    	//W:72 H:41
-	    	int w = 0;  int h = 0;
-	    	g = bi.createGraphics();
-	    	int height = bi.getHeight();
-	    	g.drawImage(bi, w, h,width, height, null);
-	    	for (int i = 0; i < bis.length; i++) {
-	    		try {  
-	    			if (status == orientation ) {w = w + width;}
-	    			else if(status == portrait){h = h + height;}
-	    			else { new Exception("status异常;你到底想往哪边合并啊");}
-	    			g.drawImage(bis[i], w, h, width, height, null);  
-	    		} catch (Exception e) {  
-	    			System.out.println(e.getMessage());  
-	    		}  
+		}
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		g = image.createGraphics();
+		for (int j = 0; j < bis.length; j++) {
+			if (status == orientation) {
+				g.drawImage(bis[j], w, h, null);
+				w = w + bis[j].getWidth();
+			} else if (status == portrait) {
+				g.drawImage(bis[j], w, h, null);
+				height = height + bis[j].getHeight();
+			} else {
+				new Exception("status异常;你到底想往哪边合并啊");
 			}
-	    	g.dispose();  
-	  
-	        return bi;  
-	    } 
-	    /**请看MergeWidth
-	     * @param bi
-	     * @param height
-	     * @param bis
-	     * @param status 合并图片的方向 orientation=0 and portrait=1.
-	     * @return
-	     */
-	    public BufferedImage MergeHeight(BufferedImage bi,int height,int status,BufferedImage... bis) {  
-	    	//W:72 H:41
-	    	int w = 0;  int h = 0;
-	    	g = bi.createGraphics();
-	    	int width = bi.getWidth();
-	    	g.drawImage(bi, w, h,width, height, null);
-	    	for (int i = 0; i < bis.length; i++) {
-	    		try {  
-	    			if (status == orientation ) {w = w + width;}
-	    			else if(status == portrait){h = h + height;}
-	    			else { new Exception("status异常;你到底想往哪边合并啊");}
-	    			g.drawImage(bis[i], w, h, width, height, null);  
-	    		} catch (Exception e) {  
-	    			System.out.println(e.getMessage());  
-	    		}  
+		}
+		g.dispose();
+
+		return image;
+	}
+
+	/**
+	 * 生成新图片保存到本地
+	 * 
+	 * @param newImage 新图片路径
+	 * @param img      合并后的图片流
+	 */
+	public void writeImageLocal(String newImage, BufferedImage img) {
+		if (newImage != null && img != null) {
+			try {
+				File outputfile = new File(newImage);
+				ImageIO.write(img, "jpg", outputfile);
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
 			}
-	    	g.dispose();  
-	  
-	        return bi;  
-	    } 
-	    
-	    
-	    /**生成新图片保存到本地
-	     * @param newImage 新图片路径
-	     * @param img 合并后的图片流
-	     */
-	    public void writeImageLocal(String newImage, BufferedImage img) {  
-	        if (newImage != null && img != null) {  
-	            try {  
-	                File outputfile = new File(newImage);  
-	                ImageIO.write(img, "jpg", outputfile);  
-	            } catch (IOException e) {  
-	                System.out.println(e.getMessage());  
-	            }  
-	        }  
-	    } 
-	        
-	    public static void main(String[] args) {  
-	        
-	    	PictureMerge tm = new PictureMerge();  
-	  
-	        BufferedImage d = tm.loadImageLocal("E:\\video\\【北酱】恋与制作人BGM三连发 主题曲+日常+悲剧收场 最后1P有彩蛋\\video1.jpg");  
-	        BufferedImage b = tm.loadImageLocal("E:\\video\\【北酱】恋与制作人BGM三连发 主题曲+日常+悲剧收场 最后1P有彩蛋\\video2.jpg");
-	        
-	        tm.writeImageLocal("C:/new1.jpg", tm.Merge(d, b));
-	        //将多张图片合在一起    
-	        System.out.println("success");  
-	    } 
+		}
+	}
+
+	public static void main(String[] args) {
+
+		PictureMerge tm = new PictureMerge();
+
+		BufferedImage d = tm.loadImageLocal("E:\\video\\【北酱】恋与制作人BGM三连发 主题曲+日常+悲剧收场 最后1P有彩蛋\\video1.jpg");
+		BufferedImage b = tm.loadImageLocal("E:\\video\\【北酱】恋与制作人BGM三连发 主题曲+日常+悲剧收场 最后1P有彩蛋\\video2.jpg");
+
+		tm.writeImageLocal("C:/new1.jpg", tm.Merge(orientation, d,b));
+		// 将多张图片合在一起
+		System.out.println("success");
+	}
 }
--- cilicili-content/src/main/java/com/cilicili/content/controller/PageController.java
+++ cilicili-content/src/main/java/com/cilicili/content/controller/PageController.java
@@ -1,12 +1,95 @@
 package com.cilicili.content.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cilicili.content.service.PageService;
 /**
  * 页面导航的Controller
  * @author  李明睿
  *
  */
 @Controller
+@RequestMapping("/to")
 public class PageController {
-	
+//
+//	/**
+//	 * 去一级主页
+//	 * 
+//	 * @return
+//	 */
+//	@RequestMapping("hone")
+//	public String toHomePage() {
+//		return "";
+//	}
+//
+//	/**
+//	 * 去二级主页
+//	 * 
+//	 * @param url
+//	 * @return
+//	 */
+//	@RequestMapping(value = "{url}", method = RequestMethod.GET)
+//	public String toSecHome(@PathVariable("url") String url) {
+//		return "";
+//	}
+//
+//	/**
+//	 * 去三级主页
+//	 * 
+//	 * @param securl
+//	 * @param thirdurl
+//	 * @return
+//	 */
+//	@RequestMapping(value = "{securl}/{thirdurl}", method = RequestMethod.GET)
+//	public String tothirdHome(@PathVariable("securl") String securl, @PathVariable("thirdurl") String thirdurl) {
+//		return "";
+//	}
+//
+//	/**
+//	 * 去某个视频页面
+//	 * 
+//	 * @param video的request URL
+//	 * @return
+//	 */
+//	@RequestMapping(value = "v/{video}", method = RequestMethod.GET)
+//	public String toVideoPage(@PathVariable("video") String video) {
+//		return "";
+//	}
+//
+//	/**
+//	 * 拿到远端IP地址
+//	 * 如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值
+//	 * X-Forwarded-For中第一个非unknown的有效IP字符串为用户的真实IP地址
+//	 * @param request
+//	 * @return
+//	 */
+//	@RequestMapping("iptest")
+//	@ResponseBody
+//	public String ipTest(HttpServletRequest request) {
+//		String removeip = "";
+//		if (request.getHeader("x-forwarded-for") == null) {
+//			removeip = request.getRemoteAddr();
+//			String remoteHost = request.getRemoteHost();
+//			int remotePort = request.getRemotePort();
+//			String remoteUser = request.getRemoteUser();
+//			System.out.println(remoteHost+"+" + remotePort +" +" + remoteUser);
+//		}else {
+//			removeip = request.getHeader("x-forwarded-for");
+//		}
+//		return removeip;
+//	}
+	@Resource
+	PageService pService;
+
+	@RequestMapping("{id}")
+	@ResponseBody
+	public int typetest(@PathVariable("id")Integer id) {
+		int i = pService.type(id);
+		return i;
+	}
 }
