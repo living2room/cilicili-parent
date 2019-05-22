483a036b369562e4b0a4bb35140549059e7cee3d
DanaOne <1297877731@qq.com>
2019-05-22 19:04:31 +0800

    实现弹创广告的增删改查

--- cilicili-web/src/main/java/com/cilicili/CiliciliWebApplication.java
+++ cilicili-web/src/main/java/com/cilicili/CiliciliWebApplication.java
@@ -1,9 +1,11 @@
 package com.cilicili;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cilicili.advertisement.mapper")
 public class CiliciliWebApplication {
 
 	public static void main(String[] args) {
